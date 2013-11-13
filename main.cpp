5f647d8dfe78e65d5d301ce69d8cb919912499a4
noodlehair <lanceton_dsouza@yahoo.co.in>
2013-11-13 03:25:43 -0500

    Added in the FlashEmitter code to generate a flash on explosion in addition to the flames already generated in the previous commit

--- main.cpp
+++ main.cpp
@@ -5,22 +5,34 @@
 #include "ImageIO.h"
 
 #define	MAX_PARTICLES	2000		
-
+#define MAX_FLASH_PARTICLES 500
 
 
 				
 bool	active=TRUE;				
 					
-
+//flame emitter variable
 float	slowdown=2.0f;				
-float	xspeed;						
-float	yspeed;						
+//float	xspeed;						
+//float	yspeed;						
 float	zoom=-20.0f;				
-
 GLuint	loop;						
 GLuint	col;						
 GLuint	delay;						
-GLuint	texture[1];					
+GLuint	texture[1];	
+
+
+//flash emitter variables
+float	flash_slowdown=1000.0f;				
+//float	xspeed;						
+//float	yspeed;						
+float	flash_zoom=-40.0f;				
+
+GLuint	flash_loop;						
+GLuint	flash_col;						
+GLuint	flash_delay;						
+GLuint	flash_texture[1];	
+
 float win_width = 768;
 float win_height = 512;
 typedef struct						
@@ -44,7 +56,7 @@ typedef struct
 particles;							// Particles Structure
 
 particles particle[MAX_PARTICLES];	// Particle Array 
-
+particles flash_particle[MAX_FLASH_PARTICLES];
 
 int LoadGLTextures(ImageIO* the_image,GLuint* texture )									
 {
@@ -116,8 +128,46 @@ void flameEmitterInit(){
 
 
 }
+
+void flashEmitterInit(){
+ImageIO* theflash_image = new ImageIO("C:/Users/Lanceton/Dropbox/compsci344_code/Final Project/Particle System Code/flash.ppm");
+	LoadGLTextures(theflash_image, &flash_texture[0]);							
+	glShadeModel(GL_SMOOTH);							
+	glClearColor(0.0f,0.0f,0.0f,0.0f);					
+	glClearDepth(1.0f);								
+	glDisable(GL_DEPTH_TEST);							
+	glEnable(GL_BLEND);									
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE);					
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT,GL_NICEST);	
+	glHint(GL_POINT_SMOOTH_HINT,GL_NICEST);				
+	glEnable(GL_TEXTURE_2D);							
+	glBindTexture(GL_TEXTURE_2D,flash_texture[0]);			
+
+	for (flash_loop=0;flash_loop<MAX_FLASH_PARTICLES;flash_loop++)				
+	{
+		flash_particle[flash_loop].active=true;								
+		flash_particle[flash_loop].life=1.0f;								
+		flash_particle[flash_loop].fade=float(rand()%100)/10000.0f +0.01;	
+		flash_particle[flash_loop].r=227/255.0;	
+		flash_particle[flash_loop].g=140.0/255.0;	;	
+		flash_particle[flash_loop].b=45/255.0;	
+		flash_particle[flash_loop].xi=float((rand()%50)-26.0f)*2;		
+		flash_particle[flash_loop].yi=float((rand()%50)-25.0f)*2;			
+		flash_particle[flash_loop].zi=float((rand()%50)-25.0f)*2;		
+		flash_particle[flash_loop].xg=0.0f;									
+		flash_particle[flash_loop].yg=0.0f;								
+		flash_particle[flash_loop].zg=0.0f;									
+	}
+
+
+
+}
+
+
 int InitGL(GLvoid)										
 {
+	
+	flashEmitterInit();
 	flameEmitterInit();
 	return TRUE;										
 }
@@ -134,7 +184,7 @@ for (loop=0;loop<MAX_PARTICLES;loop++)
 
 			
 			glColor4f(particle[loop].r,particle[loop].g,particle[loop].b,particle[loop].life);
-
+			glBindTexture(GL_TEXTURE_2D,texture[0]);	
 			glBegin(GL_TRIANGLE_STRIP);						
 			    glTexCoord2d(1,1); glVertex3f(x+1.0f,y+1.0f,z); 
 				glTexCoord2d(0,1); glVertex3f(x-1.0f,y+1.0f,z); 
@@ -175,12 +225,67 @@ for (loop=0;loop<MAX_PARTICLES;loop++)
 
 }
 
+
+void flashEmitterDisplay(){
+for (flash_loop=0;flash_loop<MAX_FLASH_PARTICLES;flash_loop++)					
+	{
+		if (flash_particle[flash_loop].active)							
+		{
+			float x=flash_particle[flash_loop].x;						
+			float y=flash_particle[flash_loop].y;						
+			float z=flash_particle[flash_loop].z+flash_zoom;					
+
+			
+			glColor4f(flash_particle[flash_loop].r,flash_particle[flash_loop].g,flash_particle[flash_loop].b,flash_particle[flash_loop].life);
+			glBindTexture(GL_TEXTURE_2D,flash_texture[0]);	
+			glBegin(GL_TRIANGLE_STRIP);						
+			    glTexCoord2d(1,1); glVertex3f(x+2.0f,y+2.0f,z); 
+				glTexCoord2d(0,1); glVertex3f(x-2.0f,y+2.0f,z); 
+				glTexCoord2d(1,0); glVertex3f(x+2.0f,y-2.0f,z); 
+				glTexCoord2d(0,0); glVertex3f(x-2.0f,y-2.0f,z); 
+			glEnd();			
+			
+			flash_particle[flash_loop].x+=	flash_particle[flash_loop].xi/(flash_slowdown);
+			flash_particle[flash_loop].y+=	flash_particle[flash_loop].yi/(flash_slowdown);
+			flash_particle[flash_loop].z+=	flash_particle[flash_loop].zi/(flash_slowdown);
+
+			flash_particle[flash_loop].xi+=	flash_particle[flash_loop].xg;			
+			flash_particle[flash_loop].yi+=	flash_particle[flash_loop].yg;			
+			flash_particle[flash_loop].zi+=	flash_particle[flash_loop].zg;			
+			flash_particle[flash_loop].life-=	flash_particle[flash_loop].fade;		
+
+			if (flash_particle[flash_loop].life<0.0f)					
+			{
+				/*particle[loop].life=1.0f;					
+				particle[loop].fade=float(rand()%100)/1000.0f+0.003f;	
+				particle[loop].x=0.0f;						
+				particle[loop].y=0.0f;						
+				particle[loop].z=0.0f;						
+				particle[loop].xi=xspeed+float((rand()%60)-32.0f);	
+				particle[loop].yi=yspeed+float((rand()%60)-30.0f);	
+				particle[loop].zi=float((rand()%60)-30.0f);	
+				particle[loop].r=1;			
+				particle[loop].g=103/255.0;			
+				particle[loop].b=0;			*/
+			}
+
+			
+
+			
+		}
+}
+
+
+}
+
+
 void  display(GLvoid)										
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		
 	glLoadIdentity();										
-	flameEmitterDisplay();
 	
+	flashEmitterDisplay();
+	flameEmitterDisplay();
     
 										
 }
