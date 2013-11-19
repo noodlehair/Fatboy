78e6d8f9117307adb9273c0fc53de3358b9b2a68
noodlehair <lanceton_dsouza@yahoo.co.in>
2013-11-19 02:35:00 -0500

    Create FlameEmitter.h

---
+++ FlameEmitter.h
@@ -0,0 +1,20 @@
+#include "LocalGraphicsLib.h"
+#include "ImageIO.h"
+#include <glut.h>
+using namespace std;
+class FlameEmitter{
+public :
+
+	 FlameEmitter(const char* file_name);
+	~FlameEmitter(void);
+	 void flameEmitterInit();
+	 void flameEmitterDisplay();
+	 ImageIO* image;
+	 float	slowdown;									
+	 float	zoom;				
+	 GLuint	loop;						
+	 GLuint	col;						
+	 GLuint	delay;						
+	 GLuint	texture[1];
+	
+};
