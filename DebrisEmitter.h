86c834ec13d9e333d8f024a252a5292260db8162
noodlehair <lanceton_dsouza@yahoo.co.in>
2013-11-19 04:07:19 -0500

    Create DebrisEmitter.h

---
+++ DebrisEmitter.h
@@ -0,0 +1,21 @@
+#include "LocalGraphicsLib.h"
+#include "ImageIO.h"
+#include <glut.h>
+using namespace std;
+class DebrisEmitter{
+public :
+
+	 DebrisEmitter(const char* file_name_1, const char* file_name_2);
+	~DebrisEmitter(void);
+	 void debrisEmitterInit();
+	 void debrisEmitterDisplay();
+	 ImageIO* image1;
+	 ImageIO* image2;
+	 float	slowdown;									
+	 float	zoom;				
+	 GLuint	loop;						
+	 GLuint	col;						
+	 GLuint	delay;						
+	 GLuint	texture[2];	
+	
+};
