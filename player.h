67877d2a028183970354a6b384f3d586542179ef
Kangyi1990 <zhangkangyi1990@gmail.com>
2013-12-06 14:57:27 -0500

    Update player.h

--- player.h
+++ player.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ImageIO.h"
  
 
 class player{
 public:
 	player(float a);
-	void draw(float x, float y, float z);
+	void draw(float x, float y, float z,GLuint texture1, GLuint texture2);
 private:
 	float size;
 
